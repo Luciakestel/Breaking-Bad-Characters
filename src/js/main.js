5ceb9f0088938418d85d882ff8edd29db9cbaf11
Luciakestel <luciakestelpires@gmail.com>
2022-11-14 13:51:15 +0100

    ready js

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,6 @@ const charactersList = document.querySelector('.js_character_list');
 const favouriteList = document.querySelector('.js_favourite_list');
 const btn = document.querySelector('.js_btn');
 const input = document.querySelector('.js_input');
-const btncloseFav = document.querySelector('.js_close');
 const btnFav = document.querySelector('.js_fav_btn');
 
 
@@ -16,19 +15,35 @@ fetch('https://breakingbadapi.com/api/characters')
 .then(dataResults =>{
     characters = dataResults;
     renderCharacters();
+    renderFavSaved();
 });
 
+function renderFavSaved(){
+  const favCharacterSave = JSON.parse(localStorage.getItem('Favourite list'));
+  if (favCharacterSave !== null){
+    favCharacters = favCharacterSave;
+    renderFavCharacters();
+  }
+}
 
 function renderCharacters(){
     let html = '';
     for (const character of characters) {
+      const characterFavIndex = favCharacters.findIndex((eachCharacterObj) => eachCharacterObj.char_id === character.char_id);
+      let classFav = '';  
+      if (characterFavIndex === -1){
+        classFav = '';
+        } else {
+          classFav = 'selected';
+        }
         html += `<li >
-        <article class="js_list_element article" id='${character.char_id}'>
+        <article class="js_list_element article ${classFav}" id='${character.char_id}'>
           <h3 class="article_title">${character.name}</h3>
           <img src="${character.img}" alt="Foto del personaje" class="article_img">
           <p class="article_status">${character.status}</p>
         </article>
       </li>`;
+    
     }
     charactersList.innerHTML = html;
     
@@ -41,7 +56,7 @@ function renderCharacters(){
 
 function handleClickCharacter(event){
     event.currentTarget.classList.toggle('selected');
-
+    console.log('he entrado');
     const selectedCharacter = characters.find((eachCharacterObj) => eachCharacterObj.char_id === parseInt(event.currentTarget.id));
     
     const characterFav = favCharacters.findIndex((eachCharacterObj) => eachCharacterObj.char_id === parseInt(event.currentTarget.id));
@@ -62,19 +77,16 @@ fetch(`https://breakingbadapi.com/api/characters?name=${searchValue}`)
     characters = dataResults;
     renderCharacters();
 });
-const searchFavCharacters = favCharacters.find((eachCharacterObj) => eachCharacterObj.name === parseInt(searchValue));
-if (searchFavCharacters === true){
-  characters.classList.add('selected');
 }
-}
+
 function renderFavCharacters(){
     let html = '';
     for (const favCharacter of favCharacters) {
         html += `<li>
-        <article class="js_list_element article selected" id='${favCharacter.char_id}'>
+        <article class="article selected" id='${favCharacter.char_id}'>
           <div class="article_box">  
             <h3 clas="article_title">${favCharacter.name}</h3>
-            <div class="article_close js_close"></div>
+            <div class="article_close js_close" id='${favCharacter.char_id}'></div>
           </div>
           <img src="${favCharacter.img}" alt="Foto del personaje" class="article_img"> 
           <p class="article_status">${favCharacter.status}</p>
@@ -84,31 +96,33 @@ function renderFavCharacters(){
     favouriteList.innerHTML = html;
     localStorage.setItem('Favourite list', JSON.stringify(favCharacters));
     
+    const btncloseFav = document.querySelectorAll('.js_close');
+    for (const buttonFav of btncloseFav) {
+      buttonFav.addEventListener(('click'), closeFav);
+    }
+  }
 
-    // btncloseFav.addEventListener(('click'), closeFav);
-    // closeFav();
+function closeFav(event){
+  event.preventDefault();
+  console.log('clickando en icono close');
+  const closeFavCharacterIndex = favCharacters.findIndex((eachCloseObj) => eachCloseObj.char_id === parseInt(event.currentTarget.id));
+  console.log(parseInt(event.currentTarget.id));
+  if (closeFavCharacterIndex !== -1) {
+    favCharacters.splice(closeFavCharacterIndex, 1);
+  }
+  renderCharacters();
+  renderFavCharacters();
 }
 
-// function closeFav(event){
-//   event.preventDefault();
-
-// }
 function resetFavs(event){
   event.preventDefault();
-  console.log('holis');
   let html = '';
   favouriteList.innerHTML = html;
+  favCharacters = [];
   localStorage.removeItem('Favourite list');
-  // for (let i = 0; i < characters.length; i++) {
-  //   if (characters[i].classList.contains('selected')){
-  //     characters[i].classList.remove('selected')
-  //   }
-  // }
-  
-  
+  renderCharacters();
 }
 
 btn.addEventListener(('click'), searchCharacters);
 btnFav.addEventListener(('click'), resetFavs);
-
 
